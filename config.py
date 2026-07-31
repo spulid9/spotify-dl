e5b9ffef8abd9ddace87dc61c75f38cb79d506f8
Sergio Pulido <sergio7pg@gmail.com>
2026-07-30 19:55:05 -0500

    Fix redirect URI to 127.0.0.1 to match Spotify dashboard

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ import os
 
 SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID", "YOUR_CLIENT_ID_HERE")
 SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET", "YOUR_CLIENT_SECRET_HERE")
-REDIRECT_URI = "http://localhost:5000/callback"
+REDIRECT_URI = "http://127.0.0.1:5000/callback"
